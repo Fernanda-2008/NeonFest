5915216694475e419d8a04f1d01b52817c994e67
Fernanda-2008 <donacianofernanda@gmail.com>
2026-05-31 18:27:17 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,33 @@
+function toggleMenu() {
+    document.getElementById("menu").classList.toggle("activo");
+}
 
+function mostrarPromo() {
+    alert("🎉 Promoción especial: 20% de descuento en boletos VIP.");
+}
+
+function seleccionarBoleto(tipo) {
+
+    document.getElementById("boletoSeleccionado").innerHTML =
+        "Has seleccionado el boleto: " + tipo;
+}
+
+function registrar() {
+
+    let nombre =
+        document.getElementById("nombre").value;
+
+    let correo =
+        document.getElementById("correo").value;
+
+    if (nombre === "" || correo === "") {
+
+        document.getElementById("respuesta").innerHTML =
+            "Por favor completa todos los campos.";
+
+        return;
+    }
+
+    document.getElementById("respuesta").innerHTML =
+        "Gracias por registrarte, " + nombre + ".";
+}
